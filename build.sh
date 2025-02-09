6a7c6208351fe641cc12075a55d36d2f37d4707a
NealVanderDoes <157188132+NealVanderDoes@users.noreply.github.com>
2025-02-08 23:39:21 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 set -o errexit # exit on error
 pip install -r requirements.txt
 python manage.py makemigrations
+python manage.py makemigrations blog
 python manage.py migrate
 python manage.py createsuperuser --noinput
